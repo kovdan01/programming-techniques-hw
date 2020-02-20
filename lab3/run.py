cdd1a3687a5deeec484f0ad35da1e6187a570419
Daniil Kovalev <kovdan01@yandex.ru>
2020-02-20 12:02:58 +0300

    Update run script

--- lab3/run.py
+++ lab3/run.py
@@ -7,22 +7,22 @@ from matplotlib import pyplot as plt
 try:
     print("Generating data...")
     if not os.path.isfile("data.sqlite"):
-        subprocess.call("../generate_data/generate_data --size=100000 --output=data.sqlite", shell=True)
+        subprocess.call("../generate_data/generate_data --size=10000000 --output=data.sqlite", shell=True)
     print("Done!")
 
     if not os.path.isfile("sizes.txt"):
         f = open("sizes.txt", "w")
-        f.write(" ".join(np.logspace(2, 5, 20).astype(int).astype(str)) + "\n")
+        f.write(" ".join(np.logspace(4, 7, 100).astype(int).astype(str)) + "\n")
         f.close()
 
     print("Running tests...")
-    if not os.path.isfile("results.csv"):
+    if not os.path.isfile("results_time.csv") or not os.path.isfile("results_collision.csv"):
         subprocess.call("./3_hashes --input=data.sqlite --sizes=sizes.txt --output_time=results_time.csv --output_collision=results_collision.csv 2> test_hash.log", shell=True)
     print("Done!")
 
     raw_time = pd.read_csv("results_time.csv", sep=';')
     data_time = dict()
-    columns_time = list(raw_time.columns_time[1:].astype(int))
+    columns_time = list(raw_time.columns[1:].astype(int))
     for i in range(raw_time.shape[0]):
         data_time[raw_time.iloc[i]["name"]] = list(raw_time.iloc[i][1:])
     plt.figure(figsize=(12, 8))
@@ -35,14 +35,13 @@ try:
     
     raw_collision = pd.read_csv("results_collision.csv", sep=';')
     data_collision = dict()
-    columns_collision = list(raw_collision.columns_collision[1:].astype(int))
+    columns_collision = list(raw_collision.columns[1:].astype(int))
     for i in range(raw_collision.shape[0]):
         data_collision[raw_collision.iloc[i]["name"]] = list(raw_collision.iloc[i][1:])
     plt.figure(figsize=(12, 8))
     for name, collisions in data_collision.items():
-        plt.plot(columns_collision, sollisions, label=name)
+        plt.plot(columns_collision, collisions, label=name)
         plt.xscale('log')
-        plt.yscale('log')
         plt.legend()
     plt.show()
 except Exception as e:
